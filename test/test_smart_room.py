e5de56b6c1be50eca812aec9031bd7bcbd398b30
pabloacinas <UO287623@uniovi.es>
2024-11-26 16:24:53 +0100

    Red phase - US3

--- test/test_smart_room.py
+++ test/test_smart_room.py
@@ -33,3 +33,17 @@ class TestSmartRoom(unittest.TestCase):
         mock_photoresistor.return_value = False
         smart_room = SmartRoom()
         self.assertFalse(smart_room.check_enough_light())
+
+    #When the room is occupied and the there is not enough light, the light should be turned on
+    @patch.object(GPIO, "output") #led
+    @patch.object(GPIO, "input") #infrared
+    @patch.object(GPIO, "input") #photoresistor
+    def test_room_is_occupied_and_not_enough_light(self, mock_photoresistor: Mock, mock_infrared: Mock, mock_led: Mock):
+        mock_photoresistor.return_value = False
+        mock_infrared.return_value = True
+        smart_room = SmartRoom()
+        smart_room.manage_light_level()
+        mock_led.assert_called_with(smart_room.LED_PIN, GPIO.HIGH)
+        self.assertTrue(smart_room.light_on)
+
+
