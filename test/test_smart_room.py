4725c8cfb2735edb247ab702e271a651064088ad
pabloacinas <UO287623@uniovi.es>
2024-11-26 16:15:25 +0100

    Red phase - US2

--- test/test_smart_room.py
+++ test/test_smart_room.py
@@ -11,8 +11,19 @@ from mock.senseair_s8 import SenseairS8
 class TestSmartRoom(unittest.TestCase):
 
     @patch.object(GPIO, "input")
-    def test_check_room_occupancy(self, mock_object: Mock):
-        # This is an example of test where I want to mock the GPIO.input() function
+    def test_check_room_occuppied(self, mock_object: Mock):
         mock_object.return_value = True
         smart_room = SmartRoom()
         self.assertTrue(smart_room.check_room_occupancy())
+
+    @patch.object(GPIO, "input")
+    def test_check_room_not_occuppied(self, mock_object: Mock):
+        mock_object.return_value = False
+        smart_room = SmartRoom()
+        self.assertFalse(smart_room.check_room_occupancy())
+
+    @patch.object(GPIO, "input")
+    def test_is_enough_light(self, mock_photoresistor: Mock):
+        mock_photoresistor.return_value = True
+        smart_room = SmartRoom()
+        self.assertTrue(smart_room.check_enough_light())
